920b8782a7470f48b28f736684e43e49b557f6ad
marcosberruhet <marcos.berruhet@gmail.com>
2024-06-03 17:37:43 -0300

    practico 8 terminado

--- practico_01/ejercicio_08.py
+++ practico_01/ejercicio_08.py
@@ -9,6 +9,10 @@ def superposicion_basico(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool
 
     Restricción: Utilizar bucles anidados.
     """
+    for elemento in lista_1:
+        for elemento2 in lista_2:
+            if elemento == elemento2:
+                return True
     pass # Completar
 
 
@@ -24,6 +28,10 @@ assert not superposicion_basico(test_list, (2, "world", 30.85))
 
 def superposicion_in(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando un sólo bucle y el operador IN."""
+    for elemento in lista_1:
+        if elemento in lista_2:
+            return True
+
     pass # Completar
 
 
@@ -41,6 +49,7 @@ def superposicion_any(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando sin bucles, el operador in y la funcion any.
     Referencia: https://docs.python.org/3/library/functions.html#any
     """
+    return any(elemento in lista_2 for elemento in lista_1)
     pass # Completar
 
 
@@ -58,6 +67,8 @@ def superposicion_set(lista_1: Iterable[Any], lista_2: Iterable[Any]) -> bool:
     """Re-Escribir utilizando conjuntos (sets).
     Referencia: https://docs.python.org/3/library/stdtypes.html#set-types-set-frozenset
     """
+    return bool(set(lista_1) & set(lista_2))
+
     pass # Completar
 
 
